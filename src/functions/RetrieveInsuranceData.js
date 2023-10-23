61050180a3c640279428e8f3799cf79a8e74fb45
Jefferson C <jcherton@hotmail.com>
2023-10-23 03:08:17 -0500

    added a new feature

--- src/functions/RetrieveInsuranceData.js
+++ src/functions/RetrieveInsuranceData.js
@@ -1,7 +1,8 @@
 const { app } = require('@azure/functions');
+const { response } = require('express');
 
-app.http('RetrieveInsuranceData', {
-    methods: ['GET', 'POST'],
+app.http('HelloWorld', {
+    methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
@@ -9,5 +10,28 @@ app.http('RetrieveInsuranceData', {
         const name = request.query.get('name') || await request.text() || 'world';
 
         return { body: `Hello, ${name}!` };
+    }
+});
+
+app.http('RetrieveInsuranceData', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+        try {
+        const { age } = await request.json();
+        
+        var object = 
+        {one: "uno", dos: "two"};
+
+        return {
+            // body: `Hello, ${msg}!`
+            body: JSON.stringify(age)
+        };
+        }
+        catch (error)
+        {
+            console.log(error);
+        }
     }
 });
